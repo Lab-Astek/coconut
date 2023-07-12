9d3a6558576c3db29e311dbf7005f70eb7c59506
Emil Pedersen <emil.pedersen@epitech.eu>
2023-07-12 13:40:46 +0200

    test: add functional tests for C3

---
+++ ftests.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+##
+## EPITECH PROJECT, 2023
+## coconut
+## File description:
+## functional tests
+##
+
+REPORT_FILE="coding-style-reports.log"
+EXPECT_FILE="expected-reports.log"
+
+IMAGE=$(docker build -q .)
+
+echo "Built image: $IMAGE"
+
+error=0
+
+for dir in ./tests/*.t; do
+    printf "\033[33mRunning test $dir\033[0m\n"
+    dirname=$(realpath "$dir")
+    docker run --rm -i \
+           -v "$dirname":/mnt/delivery \
+           -v "$dirname":/mnt/reports \
+           "$IMAGE" /mnt/delivery /mnt/reports
+
+    if diff "$dir/$EXPECT_FILE" "$dir/$REPORT_FILE"; then
+        printf "\033[32mTest $dir: OK\033[0m\n"
+    else
+        printf "\033[31mTest $dir: KO\033[0m\n"
+        error=1
+    fi
+done
+
+exit $error
