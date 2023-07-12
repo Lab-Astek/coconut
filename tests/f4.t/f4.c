48d9ca9bb59499ad805e0ee4c098da041511d168
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-12 15:38:34 +0200

    feat: basic F4 rule functional test

---
+++ tests/f4.t/f4.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2023
+** f4
+** File description:
+** f4
+*/
+
+int main(void)
+{
+    int i = 0;
+
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    i = 0;
+    return 0;
+}
