06538f5bd039037ab613db0f1cae5c2e2da10a85
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-12 17:24:40 +0200

    feat: functional test for the O3 rule

---
+++ tests/o3.t/o3.c
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2023
+** o3
+** File description:
+** o3
+*/
+
+
+void first(int *i)
+{
+    *i -= 1;
+}
+
+void second(int *i)
+{
+    *i -= 1;
+}
+
+void third(int *i)
+{
+    *i -= 1;
+}
+
+void fourth(int *i)
+{
+    *i -= 1;
+}
+
+void fifth(int *i)
+{
+    *i -= 1;
+}
+
+void six(int *i)
+{
+    *i -= 1;
+}
+
+int main(void)
+{
+    int i = 0;
+
+    first(&i);
+    second(&i);
+    third(&i);
+    fourth(&i);
+    fifth(&i);
+    six(&i);
+}
