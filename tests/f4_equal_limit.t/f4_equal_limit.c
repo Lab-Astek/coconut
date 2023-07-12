00291537ef880105d08cb75c578d58eb90237ead
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-12 15:43:29 +0200

    feat: functional test for f4 with the precise limit

---
+++ tests/f4_equal_limit.t/f4_equal_limit.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2023
+** f4
+** File description:
+** f4
+*/
+
+int main(void)
+{
+    int i = 0;
+
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    i = 2;
+    for (; i != 10; i++);
+    return 0;
+}
