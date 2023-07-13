d50e5b51ad550451a7c27269126b3fb88b9acbdf
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-13 14:34:19 +0200

    feat: more functional tests for the f5 rule

--- tests/f5.t/f5.c
+++ tests/f5.t/f5.c
@@ -10,9 +10,15 @@ void first(int *a, int *b, int *c, int *d, int *e)
     *a -= 1;
 }
 
+void sec(int *a, int *b, int *c, int *d)
+{
+    *a -= 1;
+}
+
 int main(void)
 {
     int i = 0;
 
     first(&i, &i, &i, &i, &i);
+    sec(&i, &i, &i, &i);
 }
