929965b5dc18990bb4b4dbdbb7c0fc2220e976ac
Emil Pedersen <emil.pedersen@epitech.eu>
2023-07-14 00:07:21 +0200

    docs(C2): add explanation comments

--- sources/rules/RuleC2.cpp
+++ sources/rules/RuleC2.cpp
@@ -23,25 +23,29 @@ coconut::RuleC2::RuleC2()
 {
 }
 
-void coconut::RuleC2::runCheck(ReportHandler &report,
-    clang::CompilerInstance &compiler, clang::ASTContext &context) const
+void coconut::RuleC2::runCheck(
+    ReportHandler &report, clang::CompilerInstance &compiler,
+    clang::ASTContext &context
+) const
 {
     MatchFinder finder;
+    // Handles ternary operator in a ternary operator (nested or chained)
     LambdaCallback handler([&](MatchFinder::MatchResult const &result) {
-        auto parent = result.Nodes
-            .getNodeAs<clang::ConditionalOperator>("parent");
-        auto child = result.Nodes
-            .getNodeAs<clang::ConditionalOperator>("child");
+        auto parent
+            = result.Nodes.getNodeAs<clang::ConditionalOperator>("parent");
+        auto child
+            = result.Nodes.getNodeAs<clang::ConditionalOperator>("child");
         if (!parent || !child)
             return;
-        if (parent->getBeginLoc().isMacroID() ||
-            child->getBeginLoc().isMacroID())
+        if (parent->getBeginLoc().isMacroID()
+            || child->getBeginLoc().isMacroID())
             return;
         report.reportViolation(*this, compiler, parent->getBeginLoc());
     });
+    // Handles ternary operator as expression-statement
     LambdaCallback stmtHandler([&](MatchFinder::MatchResult const &result) {
-        auto ternary = result.Nodes
-            .getNodeAs<clang::ConditionalOperator>("ternary");
+        auto ternary
+            = result.Nodes.getNodeAs<clang::ConditionalOperator>("ternary");
         if (!ternary)
             return;
         if (ternary->getBeginLoc().isMacroID())
@@ -51,24 +55,33 @@ void coconut::RuleC2::runCheck(ReportHandler &report,
 
     finder.addMatcher(
         conditionalOperator(
+            // As usual, we don't want to check header files
             isExpansionInMainFile(),
+            // Check for nested ternary operator(in both branches)
             hasDescendant(conditionalOperator().bind("child"))
-            )
-        .bind("parent"),
-        &handler);
+        )
+            .bind("parent"),
+        &handler
+    );
     finder.addMatcher(
         conditionalOperator(
+            // As usual, we don't want to check header files
             isExpansionInMainFile(),
             anyOf(
+                // If top level in an if/for/while/do/compound statement
+                // (but not in the condition of the statement)
                 hasParent(ifStmt(hasCondition(expr().bind("cond")))),
                 hasParent(forStmt(hasCondition(expr().bind("cond")))),
                 hasParent(whileStmt(hasCondition(expr().bind("cond")))),
                 hasParent(doStmt(hasCondition(expr().bind("cond")))),
                 hasParent(compoundStmt())
-                ),
+                // If parent is anything else, it's allowed
+            ),
+            // that's where we check it's not the condition of a statement
             unless(equalsBoundNode("cond"))
-            )
-        .bind("ternary"),
-        &stmtHandler);
+        )
+            .bind("ternary"),
+        &stmtHandler
+    );
     finder.matchAST(context);
 }
