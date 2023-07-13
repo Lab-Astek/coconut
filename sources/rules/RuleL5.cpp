fdb385f2ac97ed7cea381275ce2c5fe85a4d32a5
Emil Pedersen <emil.pedersen@epitech.eu>
2023-07-13 23:11:40 +0200

    feat: implement rule L5

---
+++ sources/rules/RuleL5.cpp
@@ -0,0 +1,115 @@
+/*
+** EPITECH PROJECT, 2023
+** coconut
+** File description:
+** L5
+*/
+
+#include "LambdaCallback.hpp"
+#include "rules/Rules.hpp"
+
+#include <clang/AST/ASTContext.h>
+#include <clang/AST/Decl.h>
+#include <clang/ASTMatchers/ASTMatchFinder.h>
+#include <clang/ASTMatchers/ASTMatchers.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/Casting.h>
+
+#include <unordered_set>
+
+using namespace clang::ast_matchers;
+
+coconut::RuleL5::RuleL5()
+    : Rule(
+        "MAJOR:C-L5",
+        "variables must be declared at the beginning of the function, one per "
+        "line"
+    )
+{
+}
+
+void coconut::RuleL5::runCheck(
+    ReportHandler &report, clang::CompilerInstance &compiler,
+    clang::ASTContext &context
+) const
+{
+    MatchFinder finder;
+
+    /// The set of variables that were found at the beginning of a function
+    std::unordered_set<clang::VarDecl const *> ok_vars;
+
+    /// The set of lines on which a variable was declared
+    std::unordered_set<unsigned int> lines_used;
+
+    // This matcher just finds the variables that are declared at the beginning
+    // of a function
+    LambdaCallback functions([&](MatchFinder::MatchResult const &result) {
+        auto stmt = result.Nodes.getNodeAs<clang::FunctionDecl>("function");
+        if (!stmt)
+            return;
+
+        auto body = llvm::dyn_cast<clang::CompoundStmt>(stmt->getBody());
+        if (!body)
+            return;
+
+        // Iterate over the initial variable declarations
+        // Add them to our set of ok variables
+        // The variables not in the set are the ones that are not at the
+        // beginning of the function, and they will be catched by the other
+        // matcher
+        for (auto it = body->body_begin(); it != body->body_end(); ++it) {
+            auto decl = llvm::dyn_cast<clang::DeclStmt>(*it);
+            if (!decl)
+                return;
+
+            for (auto declIt = decl->decl_begin(); declIt != decl->decl_end();
+                 ++declIt) {
+                auto varDecl = llvm::dyn_cast<clang::VarDecl>(*declIt);
+                if (!varDecl)
+                    return;
+
+                ok_vars.insert(varDecl);
+            }
+        }
+    });
+
+    // This is the actual matcher that reports violations on bad variable
+    // declarations
+    LambdaCallback variables([&](MatchFinder::MatchResult const &result) {
+        auto stmt = result.Nodes.getNodeAs<clang::VarDecl>("variable");
+        if (!stmt)
+            return;
+
+        auto loc = stmt->getLocation();
+        auto line = compiler.getSourceManager().getExpansionLineNumber(loc);
+
+        if (lines_used.find(line) == lines_used.end()) {
+            lines_used.insert(line);
+        } else {
+            // Multiple variable declarations found on the same line
+            report.reportViolation(*this, compiler, loc);
+        }
+        if (ok_vars.find(stmt) == ok_vars.end()) {
+            // Variable declaration not at the beginning of the function
+            report.reportViolation(*this, compiler, loc);
+        }
+    });
+
+    finder.addMatcher(
+        functionDecl(isDefinition(), isExpansionInMainFile()).bind("function"),
+        &functions
+    );
+
+    auto inFor = hasParent(declStmt(hasParent(forStmt())));
+    finder.addMatcher(
+        varDecl(
+            isExpansionInMainFile(), hasAncestor(functionDecl()),
+            unless(parmVarDecl()), unless(inFor)
+        )
+            .bind("variable"),
+        &variables
+    );
+    finder.matchAST(context);
+}
---
+++ tests/l5_variables.t/variables.c
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** coconut
+** File description:
+** test L5
+*/
+
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int oh_no_variables_in_the_middle(void)
+{
+    int ok = 0;
+
+    if (ok == 0) {
+        int pid = getpid();
+        printf("pid: %d\n", pid);
+        return pid;
+    }
+    int ret = -1;
+    return ret;
+}
+
+int for_loop_is_ok(void)
+{
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+int main(void)
+{
+    double d = 0.0;
+    float g, h = 1;
+    void *a = NULL;
+    int b = 0, c = 1;
+    char e = 'a'; int f = 1;
+
+    g = 1.0;
+    return d + g + h + b + c + e + f;
+}
