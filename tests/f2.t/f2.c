645f9247d8dc7672eb2208b36a018f8dee986470
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-14 12:30:59 +0200

    feat: functional tests for the F2 rule

---
+++ tests/f2.t/f2.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** o3
+** File description:
+** o3
+*/
+
+void firstTest(void)
+{
+    int i = 1;
+}
+
+void fn(void)
+{
+    int i = 0;
+}
+
+int main(void)
+{
+    firstTest();
+}
