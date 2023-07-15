2692091fb4f55cbf244ecfa654430513715816e2
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-15 15:20:36 +0200

    feat: functional test for the v1 rule Only variables and const global variables are tested (not macros yet)

---
+++ tests/v1_const_global.t/v1_const_global.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** coconut
+** File description:
+** test v1 const global
+*/
+
+#pragma once
+
+
+int const my_constant = 0;
+
+static int const my_static_constant = 0;
+
+int const my_const_array[2] = {0, 1};
