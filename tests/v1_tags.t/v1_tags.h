2d22f94156b13db9b92ca2962d1db7fe4f79f06f
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-16 17:58:41 +0200

    feat: more functional tests for the v1 rule

---
+++ tests/v1_tags.t/v1_tags.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** coconut
+** File description:
+** test v1 const global
+*/
+
+#pragma once
+
+#include <signal.h>
+
+struct myObject {
+    int a;
+};
+
+enum myEnum {
+    hello,
+    world
+};
