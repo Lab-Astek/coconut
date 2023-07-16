b2cc681fc5254eecb38d949cd5dc24cfe0cea375
Jeremy Elalouf <jeremy.elalouf@gmail.com>
2023-07-16 18:37:09 +0200

    feat: test for the v1 rule for typedefs

---
+++ tests/v1_typedef.t/v1_typedef.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** coconut
+** File description:
+** test v1 const global
+*/
+
+#pragma once
+
+typedef int my_int_t;
+
+typedef struct object_s {
+    int a;
+} object;
+
+struct my_vector {
+    int x;
+    int y;
+};
+
+typedef struct my_vector Vector;
