2e395744c946353dbeebe56f1a7debefa16e272c
Emil Pedersen <emil.pedersen@epitech.eu>
2023-07-17 17:30:57 +0200

    test(L3): multiline statements

--- tests/l3_binop.t/binop.c
+++ tests/l3_binop.t/binop.c
@@ -14,9 +14,25 @@ int make_calculations(int a, int b, int c)
     a = a +b;
     a= a + b;
     a =a + b;
-    b = (a == b) && (b == c);
-    b = (a == b) &&(b == c);
-    b = (a == b)&& (b == c);
+    b |= (a == b) && (b == c);
+    b |= (a == b) &&(b == c);
+    b |= (a == b)&& (b == c);
+    return (a + b + c);
+}
+
+int multiline(int a, int b, int c)
+{
+    b |= (a == b * c + a / c)
+        || (b * a != c / a);
+    b <<= 3;
+    b |= (a == b * c + a / c) &&
+        (b * a != c / a);
+    b <<=3;
+    b |= (a == b * c + a / c)
+        &&(b * a != c / a);
+    b<<= 3;
+    b |= (a == b * c + a / c)||
+        (b * a != (c / a));
     return (a + b + c);
 }
 
