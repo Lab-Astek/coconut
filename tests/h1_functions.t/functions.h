c986f6d57dba24188f94727eda4349bbad4d1fa2
Emil Pedersen <emil.pedersen@epitech.eu>
2023-09-20 12:29:33 +0200

    test(H1): function definitions in header

---
+++ tests/h1_functions.t/functions.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** h1
+** File description:
+** h1
+*/
+
+#pragma once
+
+int ouch_function(void)
+{
+    return (7);
+}
+
+static int make_static(void)
+{
+    return (0);
+}
+
+static inline int make_static_inline(void)
+{
+    return (0);
+}
+
+inline int make_inline(void)
+{
+    return (0);
+}
